4cf5b30c6f10280330913fba65c2ecba0c700314
achmadfarras <achmadfarras273@gmail.com>
2022-01-19 20:38:12 +0800

    Achmad Farras Shafwan - 19710091 - Tugas 1

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Latihan Appbar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu),
+        title: const Text('Hello AppBar'),
+        actions: [
+          Icon(Icons.search),
+          SizedBox(
+            width: 5,
+          ),
+          Icon(Icons.more_vert),
+        ],
+      ),
+    );
+  }
+}
